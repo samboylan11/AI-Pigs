e489c2d53c9ba9e4b4ba5fecb9f7320391419af2
Samuel Boylan-Sajous <35412345+samboylan11@users.noreply.github.com>
2020-01-03 19:38:53 -0500

    Add files via upload

---
+++ Scripts/PigAcademy.cs
@@ -0,0 +1,32 @@
+﻿//learned code from online class at immersivelimit.com
+
+using MLAgents;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PigAcademy : Academy
+{
+    private PigArea[] areas;
+
+    /// <summary>
+    /// Reset the academy
+    /// </summary>
+    public override void AcademyReset()
+    {
+        if (areas == null)
+        {
+            areas = GameObject.FindObjectsOfType<PigArea>();
+        }
+
+        foreach (PigArea area in areas)
+        {
+
+            area.numTruffles = (int)resetParameters["num_truffles"];
+            area.numStumps = (int)resetParameters["num_stumps"];
+            area.spawnRange = resetParameters["spawn_range"];
+
+            area.ResetArea();
+        }
+    }
+}
